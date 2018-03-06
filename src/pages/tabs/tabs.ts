6dbd77af9c8c13c650b01b995872f8c9ba053c1b
xiongl <xionglwh@cn.ibm.com>
2018-03-06 17:13:23 +0800

    Rename tabs name.

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { CameraPage } from '../camera/camera';
+import { SettingsPage } from '../settings/settings';
+import { GalleryPage } from '../gallery/gallery';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = GalleryPage;
+  tab2Root = CameraPage;
+  tab3Root = SettingsPage;
 
   constructor() {
 
